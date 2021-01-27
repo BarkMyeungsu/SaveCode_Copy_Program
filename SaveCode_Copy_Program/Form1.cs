226d8b9ac75514dd307403df57ccf727da1b1ee8
pmsnannan8690@gmail.com <BarkMyengsu>
2021-01-27 14:17:37 +0900

    CustomPath Patch

--- SaveCode_Copy_Program/Form1.cs
+++ SaveCode_Copy_Program/Form1.cs
@@ -4,6 +4,7 @@ using System.Windows.Forms;
 using System.IO;
 using System.Reflection;
 using System.Drawing;
+using Microsoft.WindowsAPICodePack.Dialogs;
 
 namespace SaveCode_Copy_Program
 {
@@ -25,9 +26,9 @@ namespace SaveCode_Copy_Program
             String userName = Environment.UserName;
             dirPath = @"C:\Users\" + userName + @"\Documents\Warcraft III\CustomMapData\DRD\DRDR2\";
 
-            file_check();
-            version_Load();
-            file_watcher();
+            path_check();
+ 
+
         }
 
         // 카페 홈페이지 버튼
@@ -49,40 +50,123 @@ namespace SaveCode_Copy_Program
             MessageBox.Show(saveCode+"\n붙여넣기 : Ctrl + V", "코드 복사 완료!", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
-        private void file_check()
+        private void path_check()
         {
-            // 세이브파일 디렉토리가 있는지 검사
+            // First 경로가 존재할 경우 -> 데이터 로드
             if (System.IO.Directory.Exists(dirPath))
             {
-                string[] fileDummy = Directory.GetFiles(dirPath, "[디지몬랜덤디펜스 *]*.txt");
-                // 세이브 파일이 하나도 존재하지 않는 경우
-                if (fileDummy.Length.Equals(0))
-                {
-                    noFile = true;
-                    btn_Copy.Enabled = false;
-                    
-                }
-
-                // 디렉토리 및 파일이 정상적으로 존재 하는 경우
-                else
-                {
-                    noFile = false;
-                    btn_Copy.Enabled = true;
-                    file_load();
-                }
-                file_print();
+                data_load();
             }
-            // 세이브파일 디렉토리가 존재하지 않는 경우
+            // First 경로가 존재하지 않을 경우 -> 사용자 지정 경로 확인
             else
             {
-                MessageBox.Show("세이브파일이 저장된 디렉토리를 찾을 수 없습니다.\n\n 경로 : "+dirPath, "디렉토리 경로 오류", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                Application.ExitThread();
-                Environment.Exit(0);
+                // 사용자 지정 경로가 존재할 경우
+                if(File.Exists(Application.StartupPath + @"\customPath.txt"))
+                {
+                    StreamReader sr = new StreamReader("customPath.txt");
+                    string customPath = sr.ReadLine();
+                    sr.Close();
+
+                    // 불러온 경로가 손상되지 않았는지 검사
+                    if (System.IO.Directory.Exists(customPath))
+                    {
+                        // 사용자 지정 경로가 "정상적" 으로 존재할 경우
+                        dirPath = customPath;
+                        data_load();
+                    }
+                    else
+                    {
+                        // 사용자 지정 경로가 "정상적" 으로 존재하지 않을 경우
+                        System.IO.File.Delete(Application.StartupPath + @"\customPath.txt");
+                        path_check();
+                    }
+                }
+                // 사용자 지정 경로가 존재하지 않을 경우 -> 사용자 지정 경로 설정
+                else
+                {
+                    // 사용자 지정 경로 설정? -> 예
+                    if (MessageBox.Show(
+                        "세이브파일이 저장된 경로를 찾을 수 없습니다.\n\n사용자 지정 경로를 설정 하시겠습니까?",
+                        "경로를 찾을 수 없음",
+                        MessageBoxButtons.YesNo,
+                        MessageBoxIcon.Warning) == DialogResult.Yes)
+                    {
+                        CommonOpenFileDialog folderDlog = new CommonOpenFileDialog();
+                        folderDlog.IsFolderPicker = true; // 폴더 선택
+
+                        while (true)
+                        {
+                            // 폴더 다이얼로그 -> 폴더 선택 버튼
+                            if (folderDlog.ShowDialog() == CommonFileDialogResult.Ok)
+                            {
+                                string[] selectDir = folderDlog.FileName.Split('\\');
+                                // 사용자 지정 경로를 제대로 설정한 경우
+                                if (selectDir[selectDir.Length - 1].Equals("DRDR2") && selectDir[selectDir.Length - 2].Equals("DRDR"))
+                                {
+                                    break;
+                                }
+                                // 사용자 지정 경로 설정을 제대로 하지 않았을 경우
+                                else
+                                {
+                                    MessageBox.Show("DRDR\\DRDR2 폴더가 아닙니다.\n" + folderDlog.FileName, "사용자 지정 경로 설정 오류", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                                }
+                            }
+                            // 폴더 다이얼로그 -> 취소 버튼
+                            else
+                            {
+                                if (MessageBox.Show(
+                                    "프로그램을 종료 하시겠습니까?",
+                                    "프로그램 종료",
+                                    MessageBoxButtons.YesNo,
+                                    MessageBoxIcon.Question) == DialogResult.Yes)
+                                // 사용자 지정 경로를 설정하지 않고 프로그램을 종료 시킴.
+                                {
+                                    break;
+                                    Application.Exit();
+                                }
+                            }
+                        }
+
+                        // 사용자 지정 경로를 제대로 설정한 경우 -> 사용자 지정 경로 데이터 파일 저장
+                        StreamWriter sw = new StreamWriter("customPath.txt");
+                        sw.WriteLine(folderDlog.FileName);
+                        sw.Close();
+
+                        path_check();
+                    }
+                    // 사용자 지정 경로 설정? -> 아니오
+                    else
+                    {
+                        Application.Exit();
+                    }
+                }
             }
         }
 
+        private void data_load()
+        {
+            string[] fileDummy = Directory.GetFiles(dirPath, "[디지몬랜덤디펜스 *]*.txt");
+            // 세이브 파일이 하나도 존재하지 않는 경우
+            if (fileDummy.Length.Equals(0))
+            {
+                noFile = true;
+                btn_Copy.Enabled = false;
+            }
+
+            // 디렉토리 및 파일이 정상적으로 존재 하는 경우
+            else
+            {
+                noFile = false;
+                btn_Copy.Enabled = true;
+                data_extract();
+            }
+            file_print();
+            version_Load();
+            file_watcher();
+        }
+
         // 파일을 읽어들임.
-        private void file_load()
+        private void data_extract()
         {
             // Directory Info Class 생성
             System.IO.DirectoryInfo di = new System.IO.DirectoryInfo(dirPath);
@@ -160,7 +244,7 @@ namespace SaveCode_Copy_Program
         // 프로그램에 새 세이브 코드 업데이트 적용.
         private void Changed(object source, FileSystemEventArgs e)
         {
-            file_check();
+            data_load();
             file_print();
         }
 
